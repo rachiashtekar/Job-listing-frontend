b14cf098bd1bbaebe98a8338192c5e3760676017
rachi <rachiashtekar345@gmail.com>
2024-05-23 13:22:50 +0530

    .

--- src/Home/JobSearch.js
+++ src/Home/JobSearch.js
@@ -1,49 +1,26 @@
 import React, { useCallback, useState } from "react";
 import "./JobSearch.css";
-import searchIcon from "../../src/assets/searchIcon.png"
+import searchIcon from "../../src/assets/searchIcon.png";
 import useJobContext from "../hooks/useJobContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import debounce from "lodash.debounce";
-//  import skills from "../Home/skillOptions";
- import { baseURL } from "../Components/utils/baseURL";
-
+import { baseURL } from "../Components/utils/baseURL";
 
 const JobSearch = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [selectedSkills, setSelectedSkills] = useState([]);
-  const { loggedIn, setJobListings } = useJobContext();
+ const { loggedIn, setJobListings } = useJobContext();
   const navigate = useNavigate();
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
-    getJobListings(searchTerm, selectedSkills);
+    getJobListings(searchTerm);
   };
-
-  // const handleSelectChange = (e) => {
-  //   const skill = e.target.value;
-  //   if (skill && !selectedSkills.includes(skill)) {
-  //     const updatedSkills = [...selectedSkills, skill];
-  //     setSelectedSkills(updatedSkills);
-  //     getJobListings(searchTerm, updatedSkills);
-  //   }
-  // };
-
-  // const handleRemoveSkill = (skill) => {
-  //   const updatedSkills = selectedSkills.filter((s) => s !== skill);
-  //   setSelectedSkills(updatedSkills);
-  //   getJobListings(searchTerm, updatedSkills);
-  // };
-
-  // const clearSkills = () => {     setSelectedSkills([]);
-  //   getJobListings(searchTerm, []);
-  // };
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     // Perform search operation with the search term and selected skills
     console.log("Search Term:", searchTerm);
-    console.log("Selected Skills:", selectedSkills);
   };
 
   const addJobButton = () => {
@@ -56,8 +33,7 @@ const JobSearch = () => {
         .get(`${baseURL}/api/v1/job/jobs`, {
           params: {
             searchTerm: _searchTerm,
-            skills: _selectedSkills.join(","),
-          },
+           },
         })
         .then((response) => {
           setJobListings(response.data.jobListings);
@@ -73,9 +49,9 @@ const JobSearch = () => {
     <div className="job-search">
       <form className="search-form" onSubmit={handleSearchSubmit}>
         <div className="search-bar">
-           <img src={searchIcon} alt="Search Icon" /> 
+          <img src={searchIcon} alt="Search Icon" />
           <input
-          id="search_placeholder"
+            id="search_placeholder"
             type="text"
             placeholder="Type any job title"
             value={searchTerm}
@@ -83,41 +59,12 @@ const JobSearch = () => {
           />
         </div>
       </form>
-      {/* <div className="job__search__footer">
-        <div className="select-skills">
-          <select value={selectedSkills} onChange={handleSelectChange}>
-            <option value="">Select Skill</option>
-            {skills.map((skill, index) => {
-              return <option key={index}>{skill}</option>;
-            })}
-          </select>
-          <div className="selected-skills">
-            {selectedSkills.map((skill) => (
-              <div className="selected-skill" key={skill}>
-                {skill}
-                <button
-                  className="remove-skill"
-                  onClick={() => handleRemoveSkill(skill)}
-                >
-                  X
-                </button>
-              </div>
-            ))}
-          </div>
-          {selectedSkills.length > 0 && (
-            <button className="clear__skills" onClick={clearSkills}>
-              Clear
-            </button>
-          )}
-        </div> */}
-
-        {loggedIn && (
-          <button className="add__job__btn" onClick={addJobButton}>
-            + Add Job
-          </button>
-        )}
-      </div>
-    // </div>
+      {loggedIn && (
+        <button className="add__job__btn" onClick={addJobButton}>
+          + Add Job
+        </button>
+      )}
+    </div>
   );
 };
 
